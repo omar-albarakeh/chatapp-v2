6912e6b1f41150cd73a602907c1ac607f49a7104
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-28 21:54:11 +0200

    signup page updated

--- lib/Chatapp.dart
+++ lib/Chatapp.dart
@@ -1,3 +1,4 @@
+import 'package:chatapp/Signup.dart';
 import 'package:flutter/material.dart';
 import 'loginpage.dart';
 
@@ -6,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Loginpage(),
+      home:Signuppage(),
     );
   }
 }
--- lib/Signup.dart
+++ lib/Signup.dart
@@ -10,6 +10,13 @@ class Signuppage extends StatefulWidget {
 class _SignuppageState extends State<Signuppage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+        ],
+      ),
+    );
   }
 }
