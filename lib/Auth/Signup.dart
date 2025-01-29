68ea572986d259728967e9f68a1729015bd1edc4
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-29 11:15:57 +0200

    signup page updated

--- lib/Auth/Signup.dart
+++ lib/Auth/Signup.dart
@@ -14,153 +14,21 @@ class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
           Container(
-            color: Colors.green,
-            height: 200,
             width: double.infinity,
-            padding: const EdgeInsets.symmetric(vertical: 40),
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Hello Signup",
-                  style: TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                SizedBox(width: 10),
-                Icon(Icons.handshake, size: 32, color: Colors.white),
-              ],
-            ),
+            height: double.infinity,
+            color: Colors.blue,
           ),
-          Expanded(
+          Positioned(
+            top: 300,
             child: Container(
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30)
-                )
-              ),
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const SizedBox(height: 30),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: "Email",
-                          labelStyle: const TextStyle(color: Colors.black),
-                          prefixIcon: const Icon(Icons.email, color: Colors.black),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.orange.shade500),
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(color: Colors.black),
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                        ),
-                        style: const TextStyle(color: Colors.black),
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-                      const SizedBox(height: 15),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: "Password",
-                          labelStyle: const TextStyle(color: Colors.black),
-                          prefixIcon: const Icon(Icons.lock, color: Colors.black),
-                          suffixIcon: IconButton(
-                            icon: Icon(
-                                _isPasswordVisible
-                                    ? Icons.visibility
-                                    : Icons.visibility_off,
-                                color: Colors.black),
-                            onPressed: () {
-                              setState(() {
-                                _isPasswordVisible = !_isPasswordVisible;
-                              });
-                            },
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.orange.shade500),
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(color: Colors.black),
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                        ),
-                        obscureText: !_isPasswordVisible,
-                        style: const TextStyle(color: Colors.black),
-                      ),
-                      const SizedBox(height: 15),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: "Confirm Password",
-                          labelStyle: const TextStyle(color: Colors.black),
-                          prefixIcon: const Icon(Icons.lock, color: Colors.black),
-                          suffixIcon: IconButton(
-                            icon: Icon(
-                                _isConfirmPasswordVisible
-                                    ? Icons.visibility
-                                    : Icons.visibility_off,
-                                color: Colors.black),
-                            onPressed: () {
-                              setState(() {
-                                _isConfirmPasswordVisible =
-                                !_isConfirmPasswordVisible;
-                              });
-                            },
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.orange.shade500),
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(color: Colors.black),
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                        ),
-                        obscureText: !_isConfirmPasswordVisible,
-                        style: const TextStyle(color: Colors.black),
-                      ),
-                      const SizedBox(height: 20),
-                      SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.orange.shade500,
-                            padding: const EdgeInsets.symmetric(vertical: 14),
-                            textStyle: const TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30.0),
-                            ),
-                          ),
-                          child: const Text("Sign Up",
-                              style: TextStyle(color: Colors.white)),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              decorat
+              width: 500,
+              height: 800,
+              color: Colors.red,
             ),
           ),
         ],
