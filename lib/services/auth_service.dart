a90d5e49ca02bd0546f02016d6acd2cbfca9cb93
omar albarakeh <omaralbarakeh2@gmail.com>
2025-02-01 00:21:04 +0200

    login service added

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:chatapp/Auth/loginpage.dart';
+import 'package:chatapp/HomeScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -42,4 +44,43 @@ class AuthService {
       return false;
     }
   }
-}
+
+  Future<bool> Login(BuildContext context ,String email,String password) async{
+    final url = Uri.parse('http://192.168.0.102:8000/api/auth/login');
+
+    try{
+      final response =await http.post(
+          url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+            'email':email,
+      'password':password,
+        })
+      );
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Login done!"))
+        );
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => Homescreen()),
+        );
+
+        return true;
+      }
+        else
+          {
+            ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text("Login failed!"))
+            );
+          }
+        return false;
+          }
+    catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Exception: $e"))
+      );
+      return false;
+    }
+          }
+    }
